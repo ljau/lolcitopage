f911fd673892969128d29813f0f4a860fe38f58c
Luis <luis.jauregui.fl@gmail.com>
2020-11-23 20:23:46 -0600

    building fixes

---
+++ pages/_document.jsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+                    <Head>
+
+                    </Head>
+                    <body>
+                        <Main />
+                        <NextScript />
+                    </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
